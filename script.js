274edb0bf3062a04893990c1b56b49226ba4b064
Lonkand19 <o.kandelaki@outlook.com>
2023-08-24 14:10:06 +0400

    Feat: home page stay manu

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const burgerMenu = document.querySelector(".fa-solid");
 const hiddenMenu = document.querySelector(".menu-section");
 
-burgerMenu.addEventListener("click", function () {
+burgerMenu.addEventListener("click", function ()  {
   hiddenMenu.classList.toggle("shown");
 });
